118fcbf2ef83cee0a9245aee3775ea4e9beed745
VGPS <45807150+VGPS@users.noreply.github.com>
2019-09-08 14:19:31 -0500

    edited

--- src/main/java/wgblackmon/mongodbreactivestockquoteservice/repositories/QuoteRepository.java
+++ src/main/java/wgblackmon/mongodbreactivestockquoteservice/repositories/QuoteRepository.java
@@ -4,5 +4,11 @@ package wgblackmon.mongodbreactivestockquoteservice.repositories;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import wgblackmon.mongodbreactivestockquoteservice.domain.Quote;
 
+/**
+ * 
+ * @author wgb 9/8/2019
+ *
+ */
+
 public interface QuoteRepository extends ReactiveMongoRepository<Quote, String> {
 }
